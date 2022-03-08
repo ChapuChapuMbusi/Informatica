98893f42bd4a60e7f4a6e20c6524508f93d43930
ChapuChapuMbusi <marcohorodnichev@gmail.com>
2022-03-08 18:28:21 +0100

    update registrazione

--- registrazione.php
+++ registrazione.php
@@ -2,11 +2,14 @@
 $email=$_POST['email'];
 $nome=$_POST['name'];
 $cognome=$_POST['surname'];
+$continent=$_POST['continent'];
 $genere=$_POST['sex'];
-$continent=isset($_POST['continent']) ? $_POST['continent'] : array();
+$subscribe = isset($_POST['subscribe'])? $_POST['subscribe']: 'no';
 print ("Dati inseriti:</br>");
 print ("Nome: $nome</br>");
 print ("Cognome: $cognome</br>");
-print ("Genere: $genere</br");
-print ("Continent: $continent</br>");
+print ("Origine: $continent<br>");
+print ("Genere: $genere</br>");
+print ("iscrizione: $subscribe");
 ?>
+
